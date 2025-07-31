37e2938abd631329d9964c80d80952ab4abc2ac7
JQuintero32 <jjquinterorios@gmail.com>
2025-07-31 12:36:08 -0500

    Repositorio js versión 1

---
+++ 1_sintaxis_variables/app.js
@@ -0,0 +1,44 @@
+//El alert muestra una ventana emergente en el navegador
+//alert ("Hola Mundo");
+
+
+//prompt muestra una caja para ingresar información desde el navegador
+//prompt( "Dime tu nombre")
+
+
+//Variable declarada con let
+let  personaje =  "doggy"
+
+
+let puntaje = 0
+
+
+let shots = 3
+   
+    recarga = 2
+
+
+console.log( "Total balas: ",shots + recarga)
+
+
+// el usuario capturó cuatro partos
+
+
+puntaje += 4
+
+
+// el usuario gastó dos balas
+
+
+shots -= 2
+
+
+//el usuario recargó su arma
+
+
+recarga += 1
+
+
+console.log("puntaje:" ,puntaje)
+console.log("shots:" ,shots)
+console.log("recarga:" ,recarga)
---
+++ 2_operadores_comparacion_logicos/app.js
@@ -0,0 +1,40 @@
+//Boolean(...)
+
+console.log(' "5" == "5" ', "5" == "5") //True
+console.log(' "5" === "5" ', "5" === "5") //True
+console.log(' 5 === "5" ', 5 === "5") //False
+console.log(' 5 == "5" ', 5 == "5") //True
+console.log(' 5 === (3+2) ', "5" === (3 + 2)) //False
+console.log(' 5 === 5 ', 5 === 5) //True
+
+//contatenar (+)
+console.log(5 + 3)
+console.log(5 + 3)
+console.log(5 + 3)
+
+//pida dos numeros al usuario y sumelos 
+//let num1 = Number(prompt('Dame num 1'))
+//let num2 = Number(prompt('Dame num 2'))
+
+//let resultado = num2 + num2
+//alert("El resultado de tu suma es" + resultado)
+
+//Ejercicios con javascript
+
+
+// Solicitar valores al usuario
+let valorA = prompt("Ingresa el valor A:");
+let valorB = prompt("Ingresa el valor B:");
+
+// Convertir a número
+let a = +valorA;
+let b = +valorB;
+
+// Mostrar los valores
+alert("El valor A es = (" + valorA + ") y el valor B es = (" + valorB + ")");
+
+// Resultados
+alert("La suma de (" + a + ") + (" + b + ") = (" + (a + b) + ")");
+alert("La resta de (" + a + ") - (" + b + ") = (" + (a - b) + ")");
+alert("La multiplicación de (" + a + ") * (" + b + ") = (" + (a * b) + ")");
+alert("La división de (" + a + ") / (" + b + ") = (" + (a / b) + ")");
