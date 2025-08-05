108276bf10cf3b2daa59088863154afd50890662
JQuintero32 <jjquinterorios@gmail.com>
2025-08-05 12:54:45 -0500

    Add files via upload

--- 3_condicionales/app.js
+++ 3_condicionales/app.js
@@ -47,7 +47,7 @@ if (nota < 5) {
 console.log("He obtenido un", calificacion);
 */
 
-
+/*
 //Condicional if
 let mostrarMensaje = true;
 
@@ -92,8 +92,74 @@ alert("todavia eres muy pequeño")
         alert ("piensa en cuidarte un poco mas");
     }
 
+*/
+/*
 //switch
+let nota ="A"
 
+switch (nota){
+  case "A":
 
+  alert("muy bien!") ;
+  break;
+
+  case "B":
+  alert ("Bien") ;
+  break;
+
+  case "C":
+  alert ("Mas o menos") ;
+  break
+
+  default:
+  alert("no se de que decir");
+}
+*/
+/*
+//Ejercicio 1
+//Datos usuario 
+let nombre = prompt ("Ingresa tu nombre")
+let edad = prompt ("Ingresa tu edad")
+
+console.log("Has ingresado el nombre:", nombre);
+console.log("Has ingresado la edad:", edad);
+
+// Verificamos si puede ingresar a la discoteca
+if (edad >= 18) {
+  console.log("Puedes ingresar a la discoteca.");
+} else {
+  console.log("No puedes ingresar a la discoteca.");
+}
+
+// Verificamos si puede ingresar a zona VIP
+if (nombre === "Mario" || nombre === "Carlos") {
+  console.log("Puedes ingresar a la zona VIP.");
+}
+*/
+
+//Ejercicio 2
+// Pedimos los datos al usuario
+let estatura = prompt("Ingresa tu estatura en cm:");
+let velocidad = prompt("Ingresa tu velocidad en km/h:");
+let edad = prompt("Ingresa tu edad:");
+
+console.log("Estatura:", estatura, "cm");
+console.log("Velocidad:", velocidad, "km/h");
+console.log("Edad:", edad, "años");
+
+// Verificamos si cumple con los requisitos de estatura y velocidad
+if (estatura >= 170 && velocidad >= 27) {
+  console.log("Puedes ingresar al equipo.");
+
+  // Verificamos en qué división entra
+  if (edad < 18) {
+    console.log("Vas a las divisiones menores.");
+  } else {
+    console.log("Vas al equipo profesional.");
+  }
+
+} else {
+  console.log("No cumples con los requisitos para ingresar al equipo.");
+}
 
 
