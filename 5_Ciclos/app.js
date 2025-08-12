2dd16c8f98d45a1d9da4914651aa5d5fce50e2e4
JQuintero32 <jjquinterorios@gmail.com>
2025-08-12 13:07:40 -0500

    Add files via upload

--- 5_Ciclos/app.js
+++ 5_Ciclos/app.js
@@ -127,7 +127,7 @@
 // }
 
 
-// //5
+// //5 
 
 // let inicio = prompt("Ingresa el inicio del rango a excluir:");
 // let fin = prompt("Ingresa el fin del rango a excluir:");
@@ -136,4 +136,12 @@
 //     if (i < inicio || i > fin) {
 //         console.log(i);
 //     }
+// }
+
+// //cree un ciclo for que imprima la tabla de multiplicar 7
+
+// console.log ("Tabla de multiplicar del 7:");
+
+// for (let i=1; i <=10; i++) {
+//     console.log (`7 x ${i} =${7*i}` )
 // }
